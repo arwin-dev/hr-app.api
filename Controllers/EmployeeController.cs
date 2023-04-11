c7c3647fd5ee3e0aec5a95c3690eec390d232f57
Arwin <63482587+arwin-dev@users.noreply.github.com>
2023-04-11 19:53:22 -0400

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using hr_app.api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Data.Common;
+using hr_app.api.DataContext;
+
+namespace hr_app.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MySqlManager _mySqlManager;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _mySqlManager = new MySqlManager(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            string query = "SELECT * FROM employee";
+            var employees = await _mySqlManager.GetDataTableAsync(query);
+
+            if (employees.Rows.Count == 0)
+            {
+                return NotFound();
+            }
+
+            var serializedData = JsonConvert.SerializeObject(employees);
+
+            return Content(serializedData, "application/json");
+        }
+
+    }
+}
