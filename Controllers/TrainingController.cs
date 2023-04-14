127ded60544c1e8c8938ddb4489a328d7b1813cd
Arwin <63482587+arwin-dev@users.noreply.github.com>
2023-04-14 00:59:27 -0400

    api controller added

--- Controllers/TrainingController.cs
+++ Controllers/TrainingController.cs
@@ -21,12 +21,29 @@ namespace hr_app.api.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> GetTrainingDetails()
+        {
+            string query = "SELECT t.Training_ID, t.Name FROM training t";
+
+            var trainings = await _sqlManager.GetDataTableAsync(query);
+
+            if (trainings.Rows.Count == 0)
+            {
+                return NotFound();
+            }
+
+            var serializedData = JsonConvert.SerializeObject(trainings);
+
+            return Content(serializedData, "application/json");
+        }
+
+        [HttpGet("getTraining")]
         public async Task<IActionResult> GetAllTraining(string empID)
         {
             string query = "SELECT t.Name, t.Mode, et.Start_date, et.Completion_date, et.Score FROM training t, employee_training et WHERE t.Training_ID = et.Training_ID AND et.Employee_ID = @empID";
-            var parameters = new Dictionary<string, object> 
-            { 
-                { "@empID", empID } 
+            var parameters = new Dictionary<string, object>
+            {
+                { "@empID", empID }
             };
             var trainings = await _sqlManager.GetDataTableAsync(query, parameters);
 
@@ -40,10 +57,38 @@ namespace hr_app.api.Controllers
             return Content(serializedData, "application/json");
         }
 
-/*        [HttpPost]
-        public async Task<IActionResult> AddTraining([FromBody] Training training)
+        [HttpPost("add")]
+        public async Task<IActionResult> AddTraining(Dictionary<string, object> trainingDict)
         {
+            string query = "INSERT INTO employee_training (Start_date, Completion_date, Score, Employee_ID, Training_ID) " +
+                           "VALUES (@Start_date, @Completion_date, @Score, @Employee_ID, @Training_ID)";
 
-        }*/
+            var parameters = new Dictionary<string, object>()
+            {
+                {"@Start_date", trainingDict["Start_date"]},
+                {"@Completion_date", DBNull.Value},
+                {"@Score", -1 },
+                {"@Employee_ID", trainingDict["Employee_ID"]},
+                {"@Training_ID", trainingDict["Training_ID"]},
+            };
+
+            if (trainingDict.ContainsKey("Completion_date") && trainingDict["Completion_date"] != null)
+            {
+                parameters["@Completion_date"] = trainingDict["Completion_date"];
+            }
+
+            var result = await _sqlManager.ExecuteQueryAsync(query, parameters);
+
+            if (result == 1)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
     }
 }
